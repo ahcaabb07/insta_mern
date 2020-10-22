c98a27d33b199958a55f23bc776763a589669add
Joes <veer.youssef@gmail.com>
2020-10-22 20:37:19 +0100

    compare hashed password and login

--- server/middlewares/loginMiddleware.js
+++ server/middlewares/loginMiddleware.js
@@ -1,12 +1,19 @@
+const jwt = require('jsonwebtoken');
+const hash = require('bcryptjs');
 const User = require('../models/User');
 const {SECRET} = require('../config/database');
-const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs/dist/bcrypt');
 module.exports =  (req, res, next) => {
     const {email , password} = req.body;
     User.findOne({email:email}).then(userHas => {
         !userHas ? res.status(404).json({err:'Email is incorrect!'})
-        : userHas.password !== password ?  res.status(404).json({err:'Password is incorrect!'})
-        : token = jwt.sign({_id:userHas._id} , SECRET)        
-        next();
+        : bcrypt.compare(password , userHas.password).then((match) => {
+            if(!match){
+                res.status(404).json({err:'Password is incorrect!'})
+            }else{
+                token = jwt.sign({_id:userHas._id} , SECRET)        
+                next();
+            }
+        }) 
     })
     }
